f20bf80a5a6ba69379335310d4746d25df7b6b2e
Nicholas Lepage <nic.lep@hotmail.com>
2018-06-11 02:48:49 +0000

    Finished the assignment

--- https_example.js
+++ https_example.js
@@ -3,8 +3,8 @@ var https = require('https');
 console.log('I did it!');
 
 var options = {
-  host: 'www.perdu.com',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 var callback = function(response) {
